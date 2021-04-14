a52cb07b34c4486d2d27da8884ed8707cd95a567
Matheus Felipe <mf.mf98.23@gmail.com>
2021-04-13 22:50:47 -0300

    Implementação dos slides do site

--- layout/assets/js/script.js
+++ layout/assets/js/script.js
@@ -11,26 +11,60 @@ $(document).ready(function(){
         $(this).siblings("ul").slideToggle();
     });
 
-    
+    loadProgressBar(true, true);
 
-    var $slider = $('.slider');
-    var $progressBar = $('.progress');
-    var $progressBarLabel = $( '.slider__label' );
-    
-    $slider.on('beforeChange', function(event, slick, currentSlide, nextSlide) {   
-      var calc = ( (nextSlide) / (slick.slideCount-1) ) * 100;
-      
-      $progressBar
-        .css('background-size', calc + '% 100%')
-        .attr('aria-valuenow', calc );
-      
-      $progressBarLabel.text( calc + '% completed' );
+    $(".load-page").delay(2000).fadeToggle();
+
+    $(".button-slide-products.right").on("click", function(){
+      loadProgressBar(true)
     });
-    
-    $slider.slick({
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      speed: 400
-    });  
 
-})
+    $(".button-slide-products.left").on("click", function(){
+      loadProgressBar(false)
+    });
+
+    new Glide('.banner_home').mount();
+    new Glide('.spots_slide',  {
+      perView: 1,
+      peek: {
+        before: "0",
+        after: "140"
+      },
+      swipeThreshold: false,
+    }).mount();
+});
+
+function loadProgressBar(ref, firstLoad = false){
+  var slidesProduct = $(".products .glide__slide").length;
+  var porcentageByProduct = 100/slidesProduct;
+  var progressBar = $(".progress-bar");
+  var buttonPrev = $(".button-slide-products.left");
+  var buttonNext = $(".button-slide-products.right");
+  var initialWidth = porcentageByProduct;
+
+  if(firstLoad != false){
+    progressBar.css("width", porcentageByProduct+"%");
+    buttonPrev.css("display", "none");
+    
+  }else{
+    if(ref == true){
+      porcentageByProduct += porcentageByProduct;
+      if(porcentageByProduct == 100){
+        var displayPrev = "block";
+        var displayNext = "none";
+      }
+
+    }else{
+      if(porcentageByProduct == initialWidth){
+        var displayPrev = "none";
+        var displayNext = "block";
+      }
+      porcentageByProduct -= porcentageByProduct;
+    }
+
+    $(".button-slide-products.left").css("display", displayPrev);
+    $(".button-slide-products.right").css("display", displayNext);
+
+    progressBar.css("width", porcentageByProduct+"%");
+  }
+}
